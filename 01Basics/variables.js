2c31e39890faf78b8fde8696908747e5c3aeb726
NafilaNafi <Nafila@outlook.com>
2019-03-13 14:32:50 +0530

    name changes

--- 01Basics/variables.js
+++ 01Basics/variables.js
@@ -1,6 +1,6 @@
 let name = 'Nafi'
 
-let score = 103
+let score = 100.0
 let bonus = 20
 
 let totalscore = score + bonus
@@ -8,6 +8,13 @@ let totalscore = score + bonus
 console.log(totalscore)
 
 let firstName = 'Nafila'
+firstName = 'developer'
+
 let lastName = 'Nafi'
 
+
+let fullName = firstName + '**' + lastName
+
 console.log(firstName + ' ' + lastName)
+
+console.log(fullName);
