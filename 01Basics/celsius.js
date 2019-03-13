6c88499150607a1741215ed34985071b8ce99dca
NafilaNafi <Nafila@outlook.com>
2019-03-13 15:03:51 +0530

    complex

--- 01Basics/celsius.js
+++ 01Basics/celsius.js
@@ -4,6 +4,10 @@ score = 130
 
 const bonus = 50
 
-var finalscore = (score + bonus) * 1.8
+var finalscore = (score + bonus) * 1.8 
+
+var complex = ((4 + 4) * 5) / 2
+
 
 console.log(finalscore)
+console.log(complex)
