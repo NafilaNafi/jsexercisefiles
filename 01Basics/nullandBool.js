23c4cbe46d0cf1921e65a450f0cf1ededd22c676
NafilaNafi <Nafila@outlook.com>
2019-03-13 15:54:56 +0530

    Boolean value

--- 01Basics/nullandBool.js
+++ 01Basics/nullandBool.js
@@ -6,3 +6,19 @@ var temp
 // temp = 2
 
 console.log("current temperature is: " + temp)
+
+
+//Grade system
+
+//10 - Great job
+//5 - work harder
+//2 - poor
+//0 - fail
+
+//Boolean - True/False
+
+let actualMarks = 70
+
+let mygrade = (actualMarks < 100)
+
+console.log(mygrade)
