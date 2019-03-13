44fe11f95a6676b689586e4fec4db8c320b5bbc3
NafilaNafi <Nafila@outlook.com>
2019-03-13 15:29:00 +0530

    covert temperature into fahrenheit or celsius

--- 01Basics/celsius.js
+++ 01Basics/celsius.js
@@ -9,5 +9,16 @@ var finalscore = (score + bonus) * 1.8
 var complex = ((4 + 4) * 5) / 2
 
 
-console.log(finalscore)
-console.log(complex)
+// console.log(finalscore)
+// console.log(complex)
+
+
+let tempInFahrenheit = 100
+
+// do calculation
+    // (T(°F) - 32) × 5 / 9
+
+
+let celsius = (tempInFahrenheit - 32) * 5/9
+
+console.log(celsius)
