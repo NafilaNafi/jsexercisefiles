0177be68d5d861b31efc0552d3bbbe2fa78e567d
NafilaNafi <Nafila@outlook.com>
2019-03-16 20:48:06 +0530

    email verification

--- 02Basics/learnifElse.js
+++ 02Basics/learnifElse.js
@@ -1,10 +1,28 @@
 
-if (false) {
-    console.log('I am inside if Block')
-    console.log('I am still inside if Block')
-} else if(true) {
-    console.log('I am inside IF IN CAPS BLOCK')
-} else {
-    console.log('NOTICE THIS PART')
+// if (false) {
+//     console.log('I am inside if Block')
+//     console.log('I am still inside if Block')
+// } else if(true) {
+//     console.log('I am inside IF IN CAPS BLOCK')
+// } else {
+//     console.log('NOTICE THIS PART')
 
+// }
+
+
+
+// LCO
+
+var whoIsHere = 'teacher'
+
+if (whoIsHere === 'user') {
+    console.log('Greeting message for user')
+    console.log('Allow access to view all courses')
+} else if (whoIsHere === 'teacher'){
+    console.log('Greeting message for TEACHER')
+    console.log('Allow access to his courses')
+} else {
+    console.log('MESSAGE: please verify your email')
+    console.log('send usser an email for verification')
+    
 }
