d7f730647627d376992b36c6ef0809467641bb17
NafilaNafi <Nafila@outlook.com>
2019-03-16 20:18:50 +0530

    Code Block

--- 02Basics/learnifElse.js
+++ 02Basics/learnifElse.js
@@ -1,5 +1,10 @@
 
-if (true) {
+if (false) {
     console.log('I am inside if Block')
     console.log('I am still inside if Block')
+} else if(true) {
+    console.log('I am inside IF IN CAPS BLOCK')
+} else {
+    console.log('NOTICE THIS PART')
+
 }
