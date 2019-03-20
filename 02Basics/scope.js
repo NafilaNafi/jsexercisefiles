aa3b63424c76413ce264efe1654f40a440309256
NafilaNafi <Nafila@outlook.com>
2019-03-20 11:25:51 +0530

    changes

--- 02Basics/scope.js
+++ 02Basics/scope.js
@@ -2,7 +2,11 @@ let iAmGlobal = 'somevalue'
 
 if (true) {
     let iamLocal = 'someMoreValue'
+    iAmGlobal = 'superman'
     console.log(iAmGlobal);
     console.log(iamLocal);
     
 }
+
+// console.log(iamLocal);
+console.log(iAmGlobal);
