ded50a13a820a21e0b742d0913e358a186ff9a15
NafilaNafi <Nafila@outlook.com>
2019-03-24 19:38:40 +0530

    arrays

---
+++ 02Basics/marvels.js
@@ -0,0 +1,9 @@
+//datatype  - Array
+
+
+const superHeroes = ['Iron Man', 'spiderman', 'capt. America']
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[2]);
+console.log(superHeroes.length);
