8a1931968b138af9d56336b0e586fc147b5abcd1
NafilaNafi <Nafila@outlook.com>
2019-03-24 19:48:07 +0530

    commit

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -1,9 +1,11 @@
 //datatype  - Array
 
 
-const superHeroes = ['Iron Man', 'spiderman', 'capt. America']
+const superHeroes = ['Iron Man', 'spiderman', 'capt. America', 'Thor']
 
 console.log(superHeroes);
 console.log(superHeroes[0]);
 console.log(superHeroes[2]);
-console.log([superHeroes.length]);
+console.log(superHeroes[superHeroes.length -1]);
+
+console.log('We have 3 super heroes');
