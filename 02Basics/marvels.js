d008b8495cb06b411c24721d5a5f392b63668f96
NafilaNafi <Nafila@outlook.com>
2019-03-24 19:40:38 +0530

    changes

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -6,4 +6,4 @@ const superHeroes = ['Iron Man', 'spiderman', 'capt. America']
 console.log(superHeroes);
 console.log(superHeroes[0]);
 console.log(superHeroes[2]);
-console.log(superHeroes.length);
+console.log([superHeroes.length]);
