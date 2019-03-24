67f993ecf3527576025ca5bdf908089e5984fc11
NafilaNafi <Nafila@outlook.com>
2019-03-24 19:58:14 +0530

    changes

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -8,4 +8,4 @@ console.log(superHeroes[0]);
 console.log(superHeroes[2]);
 console.log(superHeroes[superHeroes.length -1]);
 
-console.log('We have 3 super heroes');
+console.log(`We have ${3+4} super heroes`);
